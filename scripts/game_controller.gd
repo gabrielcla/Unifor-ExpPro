b732a457e8c6df4506bbb286139ad40d20a26651
Gabriel Couto <gabrielcla@edu.unifor.br>
2023-05-26 17:58:56 -0300

    Adicionado cursor controlado pelo teclado

--- scripts/game_controller.gd
+++ scripts/game_controller.gd
@@ -1,16 +1,61 @@
+"""
+Teclas:
+	Esc - Fecha o jogo
+	Setas - Movimentam o cursor 
+	Enter (do lado direito) - Clica onde o cursor estiver
+"""
+
 extends Node2D
 
+const velCursor := 5 # Velocidade do cursor
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var pos
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+# Roda em Loop
+func _process(delta):
+	pos = get_viewport().get_mouse_position() # Posição atual do mouse
+	
+	# Teclas Setas - Move o cursor do jogo
+	if Input.is_action_pressed("ui_right"):
+		pos = pos + Vector2(1,0) * velCursor
+	if Input.is_action_pressed("ui_left"):
+		pos = pos + Vector2(-1,0) * velCursor
+	if Input.is_action_pressed("ui_up"):
+		pos = pos + Vector2(0,-1) * velCursor
+	if Input.is_action_pressed("ui_down"):
+		pos = pos + Vector2(0,1) * velCursor	
+	get_viewport().warp_mouse(pos) # Move o cursor pra nova posição
+	
+	
+	# Tecla Enter - Clica com o cursor na posição atual dele
+	if Input.is_action_just_pressed("ui_ok"):
+		var a = InputEventMouseButton.new()
+		a.position = pos
+		a.button_index = 1
+		a.pressed = true
+		Input.parse_input_event(a)
+		yield(get_tree(),"idle_frame")
+		a.pressed = false
+		Input.parse_input_event(a)
+
+	# Tecla ESC - Fecha o jogo
+	if Input.is_action_pressed("ui_cancel"):
+		get_tree().quit()
+	
+
+
+func _on_Button1_pressed():
+	print("Selecionado: Botão 1")
+
+
+func _on_Button2_pressed():
+	print("Selecionado: Botão 2")
+
+
+func _on_Button3_pressed():
+	print("Selecionado: Botão 3")
+
+
+func _on_Button4_pressed():
+	print("Selecionado: Botão 4")
